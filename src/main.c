fb89692d0ecac752aa5253f06515c7aed01b1117
dasha makushunskaya <makushynskaya.darya@mail.ru>
2025-02-17 23:35:47 +0300

    fix main

--- src/main.c
+++ src/main.c
@@ -1,31 +1,35 @@
 #include "dirwalk.h"
-
 int main(int argc, char *argv[]) {
-    int show_links = 0, show_dirs = 0, show_files = 0, sort = 0;
     int opt;
-    
-    while ((opt = getopt(argc, argv, "ldfs")) != -1) {//Check params 
+    int show_links = 0, show_dirs = 0, show_files = 0, sort_entries = 0;
+    char *dir = "."; // По умолчанию текущий каталог
+    int count = 0;
+    while ((opt = getopt(argc, argv, "ldfs")) != -1) {
         switch (opt) {
-            case 'l': show_links = 1; break;
-            case 'd': show_dirs = 1; break;
-            case 'f': show_files = 1; break;
-            case 's': sort = 1; break;
-            default:
-                fprintf(stderr, "Usage: %s [-l] [-d] [-f] [-s] <dir>\n", argv[0]);
-                return EXIT_FAILURE;
+            case 'l':
+                show_links = 1;
+                break;
+            case 'd':
+                show_dirs = 1;
+                break;
+            case 'f':
+                show_files = 1;
+                break;
+            case 's':
+                sort_entries = 1;
+                break;
+	    default:
+                fprintf(stderr, "Usage: %s [-l] [-d] [-f] [dir]\n", argv[0]);
+                exit(EXIT_FAILURE);
         }
     }
-
-    if (optind >= argc) {
-        fprintf(stderr, "Error: Directory not specified\n");
-        return EXIT_FAILURE;
+    if (optind < argc) {
+        dir = argv[optind]; // Устанавливаем каталог, если он указан
     }
-
-    const char *dir = argv[optind];
-    if (!show_links && !show_dirs && !show_files) {//All params if !=
-        show_links = show_dirs = show_files = 1;
+    int total_count = 0;
+    char **entries = walk_directory(dir, show_links, show_dirs, show_files, sort_entries, &total_count);
+    if (entries != NULL) {
+        print_entries(entries, total_count);
     }
-
-    walk_dir(dir, show_links, show_dirs, show_files, sort);
     return 0;
 }
